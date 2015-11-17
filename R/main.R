9886e63df3cb43941d6e5c37c999fa27cb15515a
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2015-11-17 18:14:11 +0100

    adding Makefile and Readme

--- R/main.R
+++ R/main.R
@@ -4,16 +4,17 @@ library("DT")
 library("readr")
 library(htmlwidgets)
 
-df <- read_csv("data/Cartographie_des_donnees_logement_-_2015-01-26.csv")
-
-htmltable <- datatable(df, filter = "top", extensions = 'ColVis', options = list(dom = 'C<"clear">lfrtip'))
+carto_logement <- read_csv("data/Cartographie_des_donnees_logement_-_2015-01-26.csv")
 
 
+htmltable <- datatable(carto_logement, 
+                       filter = "top", 
+                       extensions = 'ColVis', 
+                       options = list(dom = 'C<"clear">lfrtip')
+                       )
 
+# Export HTML
 saveWidget(htmltable, 
            file = "~/Documents/projets/rapport_logement/output/table.html", 
            selfcontained = TRUE)
 
-
-
-saveWidget()
---
+++ readme.md
@@ -0,0 +1,11 @@
+
+## Rapport logement
+
+- Objectif : Naviguer dans les données de la cartographie des données du rapport Logement
+- URL : http://blaquans.github.io/rapport_logement/output/table.html
+- Source : https://www.data.gouv.fr/fr/datasets/ouverture-des-donnees-publiques-du-logement-rapport-et-cartographie/
+
+## Téléchargement des données
+
+    $ cd data/
+    $ make
