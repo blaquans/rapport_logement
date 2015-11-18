30c7dcc069eb4336e91ecd6f7281b4c64f322546
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2015-11-18 19:13:06 +0100

    update

--- R/main.R
+++ R/main.R
@@ -3,15 +3,30 @@
 library("DT")
 library("readr")
 library(htmlwidgets)
+library("dplyr")
+library(magrittr)
 
 carto_logement <- read_csv("data/Cartographie_des_donnees_logement_-_2015-01-26.csv")
+names(carto_logement) <- c("Data", "Gestionnaire", "Description", 
+               "Origine", "Accès", "Coût", "OpenData", 
+               "Granularité", "Fréquence", "Qualité", 
+               "Remarques", "Plus")
 
 
-htmltable <- datatable(carto_logement, 
-                       filter = "top", 
+carto_logement %>% names()
+carto_logement %>% str()
+
+htmltable <- carto_logement %>% datatable(. , 
+                       caption = "Cartographie des données logement", 
                        extensions = 'ColVis', 
-                       options = list(dom = 'C<"clear">lfrtip')
+                       options = list(
+                         autoWidth = FALSE,
+                         pageLength = 5,
+                         dom = 'C<"clear">lfrtip'), 
+                       rownames = TRUE, 
+                       colnames = c("Numero" = 1)
                        )
+htmltable
 
 # Export HTML
 saveWidget(htmltable, 
